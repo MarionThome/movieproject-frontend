7fa14f08d361e35ba7151c115e01ab51b3aa1a2b
Marion <thm.marion@gmail.com>
2023-01-15 12:42:06 +0100

    homepage structure and style v1

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,18 @@
 import styles from '../styles/Home.module.css';
 
+
 function Home() {
   return (
-    <div>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Moodies
         </h1>
+        <div>
+        <h2>  Let's find you something to watch tonight</h2>
+        <button className={styles.button}> Start </button>
+        </div>
       </main>
-    </div>
+
   );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,33 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
+import popcorn from "../assets/ma__s-souffl__-de-cin__ma.jpg";
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        <title>Moodies</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <Component {...pageProps} />
+      <div style={{ display: "flex" }}>
+        <div
+          style={{
+            backgroundImage: `url(${popcorn.src})`,
+            height: "100vh",
+            width: "33%",
+            //backgroundPosition : 'bottom',
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: "#a22928",
+          }}
+        ></div>
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
