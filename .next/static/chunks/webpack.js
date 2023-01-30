b5b56bfeeb80127a5efac0638ec60e25f41085fd
Marion <thm.marion@gmail.com>
2023-01-30 12:25:12 +0100

    modal responsive fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "82135855afbb2925"; }
+/******/ 		__webpack_require__.h = function() { return "861d4dfaef4e2403"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
