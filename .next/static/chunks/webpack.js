cd128264eeb6473192b53e53861c0402f4d2caf5
Marion <thm.marion@gmail.com>
2023-02-24 11:00:05 +0100

    change modal text color

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a3d3a57dd54b189c"; }
+/******/ 		__webpack_require__.h = function() { return "2c59026720eb704c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
